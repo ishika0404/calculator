7b76a1f0e397d721f4e4d0251fbcbec9e95275d9
ishika naikele <ishikanaikele@gmail.com>
2021-05-16 19:25:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.set('view engine', 'ejs');
+
+app.use( express.static( "public" ) );
+
+
+
+
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+//const url ='mongodb+srv://Ishika:ishika123@cluster0.ueoqm.mongodb.net/calculator?retryWrites=true&w=majority'
+//mongoose.connect('mongodb+srv://ishika:ishika123@cluster0.ueoqm.mongodb.net/calculator?retryWrites=true&w=majority', {useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect('mongodb+srv://Ishika:ishika123@cluster0.ueoqm.mongodb.net/calculator', {useNewUrlParser:true,useUnifiedTopology:true});
+
+const calculatorSchema={
+  name:String,
+  // ans: Number
+};
+
+const Calculator = mongoose.model("Calculator",calculatorSchema);
+
+//We stored this introduction heading in mongoDB database
+
+const calculator1=new Calculator({
+  name:"Welcome to our Calculator"
+});
+const calculator2=new Calculator({
+  name:"It perform different calculations"
+});
+
+
+
+
+
+const defaultItems=[calculator1,calculator2];
+
+const listSchema={
+  name:String,
+  items:[calculatorSchema],
+}
+const List=mongoose.model("List",listSchema);
+
+app.get('/', function(req, res) {
+  Calculator.find({},function(err,foundItems){
+    if(foundItems.length === 0){
+        Calculator.insertMany(defaultItems,function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log("Successfull saved default items to DB ");
+        }
+      });
+      res.redirect("/");
+    }else{
+      res.render('page/index', {newListItems: foundItems});
+      //res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }  
+  });
+ 
+});
+
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
+
